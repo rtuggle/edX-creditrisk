326c30b476ee413ecc45e8753447d0322b3f7abd
tugglery <ryan.tuggle@gmail.com>
2017-02-22 00:12:07 +0000

    Completed a few exercises

--- exercisesIntro.R
+++ exercisesIntro.R
@@ -1,2 +1,27 @@
 #Credit Risk Course
 ##Introductory exercises 
+###Exercise 1
+a <- 2 + 3^4 - (1/2^3) + 3 * 5
+b <- exp((2*(2+7)) / 3)
+c <- log(3^2 - 1)
+d <- exp(pi - 2) - 1
+sum(a,b,c,d)
+
+###Exercise 2
+x <- seq(1,101,2)
+y <- x + 1
+z <- x^3
+
+w <- union(x,y)
+w <- sort(w)
+wX <- w[1:(length(w) - 2)]
+
+wX0 <- sapply(wX, function(x) ifelse(x < 80, x, 0))
+
+###Exercise 3
+dN <- rnorm(2000, 2, 3)
+dN7 <- dN[dN > 7]
+length(dN7)
+length(dN7) / length(dN)
+
+qplot(dN7, geom = 'histogram', fill = I('red'), main = "Outliers")
